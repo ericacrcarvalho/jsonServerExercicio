cceb1cc0ec8054ec0649a1c76f3af5a38c29b42b
Erica Carvalho <ericacrcarvalho@gmail.com>
2021-11-05 22:22:47 -0300

    resolucao

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -14,17 +14,102 @@ const Settings = () => {
   // Edit password
   const [idEdit, setIdEdit] = useState("");
   const handleEdit = () => {
-    openModalize();
+    if (idEdit.length != 0) {
+      fetch(`${URL}/users?id=${idEdit}`, {
+        method: "GET",
+        headers: {
+          'content-type': 'application/json'
+        }
+      })
+      .then((response) => response.json())
+      .then((response) => {
+        if (response.length != 0) {
+          openModalize();
+        }
+        else {
+          alert("Usuário não cadastrado.");
+
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+    }
+    else {
+      alert("Favor inserir o id.")
+    }
   }
 
   // PUT / PATCH
   const [newPassword, setNewPassword] = useState("");
   const handleUpdate = () => {
+    if (newPassword.length != 0) {
+      const updateUser = {
+        "password": newPassword
+      }
+      fetch(`${URL}/users/${idEdit}`, {
+        method: "PATCH",
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify(updateUser)
+      })
+      .then((response) => {
+        response.json();
+        alert("Senha alterada com com sucesso.");
+        setIdEdit("");
+      })
+      .catch((error) => {
+        console.error(error);
+      })  
+      modalizeRef.current?.close();
+    }
+    else {
+      alert("Você deve inserir a senha.")
+    }
+    setNewPassword("");
   }
 
   // Delete user
   const [idDelete, setIdDelete] = useState("");
     const handleDelete = () => {
+      if (idDelete.length != 0) {
+        fetch(`${URL}/users?id=${idDelete}`, {
+          method: "GET",
+          headers: {
+            'content-type': 'application/json'
+          }
+        })
+        .then((response) => response.json())
+        .then((response) => {
+          if (response.length != 0) {
+            fetch(`${URL}/users/${idDelete}`, {
+              method: "DELETE",
+              headers: {
+                'content-type': 'application/json'
+              }
+            })
+            .then((response) => {
+              response.json();
+              alert("Usuário deletado com sucesso");
+            })
+            .catch((error) => {
+              console.error(error);
+            })        
+          }
+          else {
+            alert("Usuário não cadastrado.");
+
+          }
+          setIdDelete("");
+        })
+        .catch((error) => {
+          console.error(error);
+        })
+    }
+    else {
+      alert("Favor inserir o id.")
+    }
   }
 
   return (
